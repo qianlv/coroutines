d3be7815319a8bc2e295a50b4a8539769bd5c241
qianlv <qianlv7@qq.com>
2015-11-05 11:09:15 +0800

    测试socket封装

--- coroutines/test_client.py
+++ coroutines/test_client.py
@@ -1,19 +1,27 @@
 # encooding=utf-8
 
 import socket
-from coroutines import Socket
+import sys
+from coroutines import Connection
 from coroutines import Scheduler
+from coroutines import read
+
 
 
 def client(host, name):
-    rawsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    rawsock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
     rawsock.connect((host, name))
-    sock = Socket(rawsock)
-    for i in range(10):
-        yield sock.sendall('number{}'.format(i))
+    sock = Connection(rawsock, (host, name))
+    while True:
+        data = yield read(sys.stdin)
+        print '1', data
+        if not data:
+            break
+        yield sock.sendall(data)
         data = yield sock.recv(1024)
+        print '2', data
 
 if __name__ == '__main__': 
     sched = Scheduler()
-    sched.new(client('127.0.0.1', 8000))
+    sched.new(client('ip6-localhost', 8000))
     sched.mainloop()
